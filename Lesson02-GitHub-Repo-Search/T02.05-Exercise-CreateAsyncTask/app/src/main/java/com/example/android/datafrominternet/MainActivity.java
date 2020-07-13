e4986fabca28446e5bef404005c432528fdd0af3
jagenau49 <jagenau49@gmail.com>
2020-07-13 11:17:12 +0200

    Async method repaired.

--- Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -62,8 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
         // COMPLETED (4) Create a new GithubQueryTask and call its execute method, passing in the url to query
 
-        GithubQueryTask mGithubQueryTask=new GithubQueryTask();
-        mGithubQueryTask.execute(githubSearchUrl);
+        new GithubQueryTask().execute(githubSearchUrl);
     }
 
     // COMPLETED (1) Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
